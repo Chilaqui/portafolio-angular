1aea4d4afe8f1545fbfaf0d4a49b14f2667deea5
Hector <comhector_@hotmail.com>
2025-07-25 08:01:40 -0600

    integracion de componentes

--- portafolio-front-end/src/app/app-module.ts
+++ portafolio-front-end/src/app/app-module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HeaderComponent } from './components/header-component/header-component';
+import { FooterComponent } from './components/footer-component/footer-component';
+import { ToggleThemeComponent } from './components/toggle-theme-component/toggle-theme-component';
+import { ProjectCardComponent } from './components/project-card-component/project-card-component';
+import { HomeComponent } from './routes/home-component/home-component';
+import { ProjectsComponent } from './routes/projects-component/projects-component';
+import { ContactComponent } from './routes/contact-component/contact-component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HeaderComponent,
+    FooterComponent,
+    ToggleThemeComponent,
+    ProjectCardComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ portafolio-front-end/src/app/components/toggle-theme-component/toggle-theme-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-theme-component',
+  standalone: false,
+  templateUrl: './toggle-theme-component.html',
+  styleUrl: './toggle-theme-component.css'
+})
+export class ToggleThemeComponent {
+
+}
