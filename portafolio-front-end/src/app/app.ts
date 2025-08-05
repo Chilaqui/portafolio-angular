0c9caad9dee1e8a1cae399bfce7ff19ba1c65760
Hector <comhector_@hotmail.com>
2025-08-05 17:17:33 -0600

    modifiacando para que el fotter aparesca abajo de todo el contenido y no tenga nada que ver con los cambios de rutas

--- portafolio-front-end/src/app/app.ts
+++ portafolio-front-end/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'portafolio-front-end';
+  
+  
 }
